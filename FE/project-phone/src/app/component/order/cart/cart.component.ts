3d3e82b671a7d123c4d5336d4cf91c69e6b953e8
unknown <vanhungns96@gmail.com>
2023-01-05 17:22:04 +0700

    update

--- FE/project-phone/src/app/component/order/cart/cart.component.ts
+++ FE/project-phone/src/app/component/order/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-cart',
@@ -10,6 +11,23 @@ export class CartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // Product Quantity
+    $('.quantity button').on('click', function () {
+      var button = $(this);
+      var oldValue = button.parent().parent().find('input').val();
+      if (button.hasClass('btn-plus')) {
+        // @ts-ignore
+        var newVal = parseFloat(oldValue) + 1;
+      } else {
+        if (oldValue > 0) {
+          // @ts-ignore
+          var newVal = parseFloat(oldValue) - 1;
+        } else {
+          newVal = 0;
+        }
+      }
+      button.parent().parent().find('input').val(newVal);
+    });
   }
 
 }
--- FE/project-phone/src/app/component/product/detail/detail.component.ts
+++ FE/project-phone/src/app/component/product/detail/detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -10,6 +10,23 @@ export class DetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // Product Quantity
+    $('.quantity button').on('click', function () {
+      var button = $(this);
+      var oldValue = button.parent().parent().find('input').val();
+      if (button.hasClass('btn-plus')) {
+        // @ts-ignore
+        var newVal = parseFloat(oldValue) + 1;
+      } else {
+        if (oldValue > 0) {
+          // @ts-ignore
+          var newVal = parseFloat(oldValue) - 1;
+        } else {
+          newVal = 0;
+        }
+      }
+      button.parent().parent().find('input').val(newVal);
+    });
   }
 
 }
